760f0bda4494783b3b9a11709bbbe29ee98d791c
Kirk Sudduth <kirksudduth@gmail.com>
2020-09-15 11:49:02 -0500

    got navbar built as much out as i want right now and also added activity notes to home page

--- pawpalproject/pawpalapp/views/activity_types/list.py
+++ pawpalproject/pawpalapp/views/activity_types/list.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
-from ...models import ActivityType
+from ...models import ActivityType, Activity
 
 @login_required
 def activity_type_list(request):
@@ -9,11 +9,15 @@ def activity_type_list(request):
         print("USER:", request.user.id)
         # add pawPal_id=request.user.id ?? in filter.() OR something like that
         all_activity_types = ActivityType.objects.filter()
-        
+        all_activities = Activity.objects.filter()
+        list_view = {}
+        list_view['all_activity_types'] = ActivityType.objects.filter()
+        list_view['all_activities'] = Activity.objects.filter()
+
         template_name = 'activity_types/list.html'
 
         context = {
-            'all_activity_types': all_activity_types
+            'list_view' : list_view
         }
 
         return render(request, template_name, context)
