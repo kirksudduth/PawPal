0286139d7cb687d7e96eb325ead40bcf58c19979
Kirk Sudduth <kirksudduth@gmail.com>
2020-09-15 15:09:33 -0500

    got some unique data rendering on the page, but not displaying how i want it

--- pawpalproject/pawpalapp/views/activity_types/list.py
+++ pawpalproject/pawpalapp/views/activity_types/list.py
@@ -1,18 +1,20 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
-from ...models import ActivityType, Activity
+from ...models import ActivityType, Activity, ParentPawPal
 
 @login_required
 def activity_type_list(request):
     if request.method == 'GET':
-        print("USER:", request.user.id)
-        # add pawPal_id=request.user.id ?? in filter.() OR something like that
-        all_activity_types = ActivityType.objects.filter()
-        all_activities = Activity.objects.filter()
+        parent_pawpal_set = ParentPawPal.objects.filter(parent_id=request.user.id)
+        print("What is this?", parent_pawpal_set)
         list_view = {}
-        list_view['all_activity_types'] = ActivityType.objects.filter()
-        list_view['all_activities'] = Activity.objects.filter()
+        list_view['all_activity_types'] = ActivityType.objects.filter(pawpal_id=parent_pawpal_set[0].pawpal_id)
+        list_view['all_activities'] = Activity.objects.filter(pawpal_id=parent_pawpal_set[0].pawpal_id)
+
+        # for pp in parent_pawpal_set:
+        #     list_view['all_activity_types_{pp.pawpal.name}'] = ActivityType.objects.filter(pawpal_id=parent_pawpal_set[0].pawpal_id)
+        #     list_view['all_activities_{pp.pawpal.name}'] = Activity.objects.filter(pawpal_id=parent_pawpal_set[0].pawpal_id)
 
         template_name = 'activity_types/list.html'
 
