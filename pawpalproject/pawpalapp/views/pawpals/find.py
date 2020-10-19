008017d4dc273f5d2b2ba6374302e83d92521b65
Kirk Sudduth <kirksudduth@gmail.com>
2020-10-18 22:23:40 -0500

    added a select/option pawpal element in the profile page

--- pawpalproject/pawpalapp/views/pawpals/find.py
+++ pawpalproject/pawpalapp/views/pawpals/find.py
@@ -18,9 +18,12 @@ def find_pawpal(request):
 
     if request.method == "POST":
         form_data = request.POST
-        pawpal = PawPal.objects.get(id=form_data.id)
+        print("form_data:", form_data)
+        pawpal = PawPal.objects.get(id=form_data['pawpal'][0])
         parent = Parent.objects.get(id=request.user.id)
         ParentPawPal.objects.create(
             pawpal=pawpal,
             parent=parent
         )
+
+        return redirect(reverse('pawpalapp:pawpal_details', args=[form_data['pawpal'][0]]))
