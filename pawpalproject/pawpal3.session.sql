14dab45844be8186ad73ad6373ba4e5a13a2bd9e
Kirk Sudduth <kirksudduth@gmail.com>
2020-11-08 22:52:34 -0600

    i am not sure what i did to jack this up

--- pawpalproject/pawpal3.session.sql
+++ pawpalproject/pawpal3.session.sql
@@ -1,3 +1,15 @@
 delete from pawpalapp_parentpawpal
 where id = 15;
 
+DELETE 
+FROM django_migrations
+WHERE id = 20;
+DELETE 
+FROM django_migrations
+WHERE id = 21;
+DELETE 
+FROM django_migrations
+WHERE id = 22;
+DELETE 
+FROM django_migrations
+WHERE id = 23;
