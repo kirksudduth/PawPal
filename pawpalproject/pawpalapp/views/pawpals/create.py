bfe8fd92c1df693d3cd81773c3a485da98e1b5d9
Kirk Sudduth <kirksudduth@gmail.com>
2020-12-01 23:09:58 -0600

    going through codemy upload image with django tutorial again

--- pawpalproject/pawpalapp/views/pawpals/create.py
+++ pawpalproject/pawpalapp/views/pawpals/create.py
@@ -4,12 +4,12 @@ from django.views.decorators.csrf import csrf_exempt
 from ...models import PawPal, ParentPawPal, Parent, ActivityType
 from ...forms.pawpal.add_form import AddPawPalForm
 
+
 @csrf_exempt
 def create_pawpal(request):
 
     if request.method == "POST":
         form = AddPawPalForm(request.POST)
-        print("FORM-->", form.is_valid())
         if form.is_valid():
             form.save()
             created_pawpal = PawPal.objects.all().last()
@@ -18,7 +18,8 @@ def create_pawpal(request):
             ActivityType.objects.create(title="Walk", pawpal=created_pawpal)
             ActivityType.objects.create(title="Wash", pawpal=created_pawpal)
             ActivityType.objects.create(title="Meds", pawpal=created_pawpal)
-            ActivityType.objects.create(title="Miscellaneous", pawpal=created_pawpal)
+            ActivityType.objects.create(
+                title="Miscellaneous", pawpal=created_pawpal)
             parent = Parent.objects.get(user_id=request.user.id)
             ParentPawPal.objects.create(pawpal=created_pawpal, parent=parent)
             return redirect("/")
