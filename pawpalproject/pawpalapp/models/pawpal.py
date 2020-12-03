7f462d787b154bdb9aa3db9c9836797a4aff438d
Kirk Sudduth <kirksudduth@gmail.com>
2020-12-02 23:20:27 -0600

    trying to figure out what the images are doing when pawpals are created

--- pawpalproject/pawpalapp/models/pawpal.py
+++ pawpalproject/pawpalapp/models/pawpal.py
@@ -2,12 +2,12 @@ from django.db import models
 
 
 class PawPal(models.Model):
-    
+
     name = models.CharField(max_length=30)
     birth_date = models.DateField()
     favorite_treat = models.CharField(max_length=30)
     favorite_toy = models.CharField(max_length=30)
     image = models.ImageField(upload_to='images/', null=True, blank=True)
 
-    def __str__(self):
-        return self.name
+    # def __str__(self):
+    #     return self.name
