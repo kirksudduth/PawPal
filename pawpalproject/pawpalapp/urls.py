cd8ae9833089eda44358a5f07f6641f461d606bd
Kirk Sudduth <kirksudduth@gmail.com>
2020-12-05 21:30:09 -0600

    deleted excess code from urls to try and figure out why images are not posting

--- pawpalproject/pawpalapp/urls.py
+++ pawpalproject/pawpalapp/urls.py
@@ -13,16 +13,17 @@ urlpatterns = [
     path('', pawpal_list, name='home'),
     path('pawpals/find', find_pawpal, name='find_pawpal'),
     path('pawpals/<int:pawpal_id>', pawpal_details, name='pawpal_details'),
-    path('pawpals/<int:pawpal_id>/add_message', add_message, name='add_message'),
-    path('pawpals/<int:pawpal_id>/message_board', message_list, name='message_list'),
-    path('pawpals/<int:pawpal_id>/add_activity_type', add_activity_type, name='add_activity_type'),
+    path('pawpals/<int:pawpal_id>/add_message',
+         add_message, name='add_message'),
+    path('pawpals/<int:pawpal_id>/message_board',
+         message_list, name='message_list'),
+    path('pawpals/<int:pawpal_id>/add_activity_type',
+         add_activity_type, name='add_activity_type'),
     path('profile/', profile, name='profile'),
     path('profile/edit', profile_edit, name='profile_edit'),
     path('profile/dedpal', delete_pawpal, name='delete_pawpal'),
-    path('activity_types/<int:activity_type_id>', activity_type_details, name='activity_type_details'),
-    path('activity_types/<int:activity_type_id>/add_activity', create_activity, name='add_activity'),
-] 
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('activity_types/<int:activity_type_id>',
+         activity_type_details, name='activity_type_details'),
+    path('activity_types/<int:activity_type_id>/add_activity',
+         create_activity, name='add_activity'),
+]
