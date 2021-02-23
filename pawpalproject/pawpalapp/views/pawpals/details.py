42682feb97d44eff542a1cf9cdf55452ad2dfb9a
Kirk Sudduth <kirksudduth@gmail.com>
2021-02-23 16:05:39 -0600

    wrapped the activity types list in a container

--- pawpalproject/pawpalapp/views/pawpals/details.py
+++ pawpalproject/pawpalapp/views/pawpals/details.py
@@ -2,22 +2,21 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from ...models import PawPal, ActivityType, Activity, Message
 
+
 @login_required
 def pawpal_details(request, pawpal_id):
     pawpal = PawPal.objects.get(id=pawpal_id)
     activity_types = list(ActivityType.objects.filter(pawpal_id=pawpal_id))
     activities = Activity.objects.filter(pawpal_id=pawpal_id)
     messages = Message.objects.filter(pawpal_id=pawpal_id).order_by('when')
-    
-
 
     template = 'pawpals/details.html'
 
     context = {
-        'pawpal' : pawpal,
-        'activity_types' : activity_types,
-        'activities' : activities,
-        'messages' : messages,
+        'pawpal': pawpal,
+        'activity_types': activity_types,
+        'activities': activities,
+        'messages': messages,
     }
 
     return render(request, template, context)
